cc9af36466c42b39788f57b52e245340b07fc011
Lucile <developer.lvt@gmail.com>
2021-04-07 22:46:28 -0400

    switched to callback pattern

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-$("#fetch").on("click", function(){
-    $("#result").text("Loading...")
-    $("#fetch").attr("disabled",true)
-    fetchData()
-})
+$("#fetch").on("click", function () {
+  $("#result").text("Loading...");
+  $("#fetch").attr("disabled", true);
+  fetchData("IBM", (res) => {
+    $("#result").text(res);
+    $("#fetch").attr("disabled", false);
+  });
+});
 
-function fetchData(){
-    fetch("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=ZCER87CRU4VD7SK1")
-    .then(function(response){
-        return response.json();
-    }).then(function(data){
-        $("#result").text(JSON.stringify(data["Global Quote"]["05. price"]))
-        $("#fetch").attr("disabled",false)
+function fetchData(symbol, resultCallBack) {
+  fetch(
+    `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=demo`
+  )
+    .then(function (response) {
+      return response.json();
     })
+    .then(function (data) {
+      var result = JSON.stringify(data["Global Quote"]["05. price"]);
+      resultCallBack(result);
+    });
 }
