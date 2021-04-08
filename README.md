3411fb07ed9eef7af651e4b947859438044da418
Lucile <developer.lvt@gmail.com>
2021-04-08 11:05:53 -0400

    updated readme and added comment to js files

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## General overview
 1. When the button is clicked, it's calling the function fetchData(symbol,resultCallBack) declared in api.js
+The second parameter of that function in this example (**res**) "stores" the result from the callback function declared.
+
 ```
 $("#fetch").on("click", function () {
   $("#result").text("Loading...");
@@ -14,3 +16,6 @@ $("#fetch").on("click", function () {
   }
 });
 ```
+
+2. The fetchData function declared in api.js takes two parameters. 
+It handles cache saving (if an url has already been searched, it won't make an api call again and it will take the result from the cache) and therefore prevents making multiple api calls on the same url. It also has a queue url search logic where a same url endpoint cannot be called multiple times at the same time, it will wait for the previous call to finish (with setTimeout)
--- api.js
+++ api.js
@@ -1,9 +1,12 @@
 function fetchData(symbol, resultCallBack) {
   var url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=demo`;
+  // if the url has already been searched and therefore if found in the apiResultCache object, the value will be passed as an argument in the resultCallBack(value)
   if (apiResultCache[url]) {
     console.log("cache entry found!");
     resultCallBack(apiResultCache[url]);
+  // else, if has never been searched before and therefore if not found in the apiResultCache object, the fetch() is fired and the key-value pair (url-searched:price-resulting-from-the-fetch) is saved in the apiResultCache
   } else {
+    // The following if-else statement prevent the same url from being searched multiple times at the same time. If url already being searched (and therefore in the queue), then it will be searched only 16ms later (via setTimeout)
     if (urlInProgress.indexOf(url, 0) === -1) {
       urlInProgress.push(url);
       fetch(url)
@@ -26,6 +29,8 @@ function fetchData(symbol, resultCallBack) {
   }
 }
 
+// object that will store the url already searched and the result of the fetch api. Key-value pair is url:result-from-fetch
 var apiResultCache = {};
 
+// temporary queue list of urls currently in process in the fetchData
 var urlInProgress = [];
