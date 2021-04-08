c1be617448cf9dafe7e8223409f771d2e180acae
Lucile <developer.lvt@gmail.com>
2021-04-07 22:52:32 -0400

    extracted api function in dedicated file

--- index.js
+++ index.js
@@ -6,16 +6,3 @@ $("#fetch").on("click", function () {
     $("#fetch").attr("disabled", false);
   });
 });
-
-function fetchData(symbol, resultCallBack) {
-  fetch(
-    `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=demo`
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      var result = JSON.stringify(data["Global Quote"]["05. price"]);
-      resultCallBack(result);
-    });
-}
